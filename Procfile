ca6f1cc2da2ceb9e4d777118cdd3a4aedfd70856
dalor <dalor@i.ua>
2019-03-22 02:10:50 +0200

    del gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn core:app
+web: python core.py
--- core.py
+++ core.py
@@ -216,5 +216,5 @@ def webhook():
     return 'ok', 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 
